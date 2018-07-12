a1f150f50955273dab2daf57815f95134fa77643
simplyshravan <shravan.simply@outlook.com>
2018-07-12 19:24:29 +0530

    Changes in pyramid program
    shorter from of pyramid

--- pyramid.py
+++ pyramid.py
@@ -1,20 +1,13 @@
-def pyramid(i):
-   for j in range(i):
-            for z in range(i-j):
-                    print(' ',end='')
-            for k in range(j+1):
-                    print('* ',end='')
-            print('')
+#def pyramid(i):
+#   for j in range(i):
+#            for z in range(i-j):
+#                    print(' ',end='')
+#            for k in range(j+1):
+#                    print('* ',end='')
+#            print('')
 
-inp=eval(input("enter the height of pyramid:"));
-#inp1=input();
-pyramid(inp);
-#print(List(inp1));
-
-#for i in inp1:
-#    print(i)
-
-#while input()!='':
-#    k=input()
-#    print(k)
-
+#inp=eval(input("enter the height of pyramid:"));
+#pyramid(inp);
+inp=int(input())
+for i in range(inp):
+    print(' '*(inp-1-i)+'* '*(i+1));
--- pyramid_full.py
+++ pyramid_full.py
@@ -1,16 +1,22 @@
-def pyramid(i):
-   for j in range(i):
-            for z in range(i-j):
-                    print(' ',end='')
-            for k in range(j+1):
-                    print('* ',end='')
-            print('')
-   for j in range(1,i):
-            for k in range(j+1):
-                    print(' ',end='')
-            for z in range(i-j):
-                    print('* ',end='')
-            print('')
+#def pyramid(i):
+#   for j in range(i):
+#            for z in range(i-j):
+#                    print(' ',end='')
+#            for k in range(j+1):
+#                    print('* ',end='')
+#            print('')
+#   for j in range(1,i):
+#            for k in range(j+1):
+#                    print(' ',end='')
+#            for z in range(i-j):
+#                    print('* ',end='')
+#            print('')
 
-inp=eval(input("enter the height of pyramid:"));
-pyramid(inp);
+#inp=eval(input("enter the height of pyramid:"));
+#pyramid(inp);
+
+inp=int(input())
+for i in range(inp):
+    print(' '*(inp-1-i)+'* '*(i+1));
+for i in range(inp-1):
+    print(' '*(i+1)+'* '*(inp-1-i));
