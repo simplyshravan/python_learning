f0fbbdd03e17df8773d58e509b64fd1ff549b757
Shravan Yadav <shravan.simply@outlook.com>
2018-10-24 16:44:15 +0530

    updated code
    updated code

--- kaggle_bakery_analysis.py
+++ kaggle_bakery_analysis.py
@@ -4,15 +4,35 @@ import seaborn as sns
 import numpy as np
 
 df=pd.read_csv(r'BreadBasket_DMS.csv')
-print(df.head())
+#print(df.head())
+#print(df.info())
+#print(df.describe())
+month_col=df['Date'].values
+month_col= [ my_str.split("-")[1] for my_str in month_col ]
+df['Month']=month_col
+month_col=df['Date'].values
+month_col= [ my_str.split("-")[0] for my_str in month_col ]
+#print(month_col)
+df['Year']=month_col
 print(df.info())
-print(df.describe())
+print(df.head())
 
-group_dt=df.groupby('Date')
+
+
+group_dt=df.groupby(['Year','Month']).agg(np.size)
 #print(group_dt.agg(np.size))
+print(group_dt.head())
+#group_month=df.groupby(['Date','Month'])
+#group_dt.agg(np.size)
+#print(group_by_dt.info())
+#print(group_by_dt.head())
+print(group_dt['Transaction'].max())
 
-group_by_dt=group_dt.agg(np.size)
+print(group_dt[group_dt['Transaction']==group_dt['Transaction'].max()])
 
-print(group_by_dt.head())
+#sns.pairplot(group_dt)
 
-print(group_by_dt.loc[group_by_dt['Transaction'].max()])
+#sns.distplot(group_dt['Transaction'])
+sns.countplot(x=df['Year'],hue=df['Transaction'],data=df)
+#group_dt=df.groupby(['Date','Month'])
+
